cb7746f367e0a37abc967b75721d7d1d62d5e630
Hector Rincon <hecerinc@hotmail.com>
2017-02-21 01:02:32 -0600

    Make TodoItem more succinct

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,20 +2,12 @@ import React from 'react';
 import TodoActionCreator from '../actions/TodoActionCreator';
 
 class TodoItem extends React.Component {
-	constructor() {
-		super();
-		this.checkChange = this.checkChange.bind(this);
-	}
-
-	checkChange(event) {
-		TodoActionCreator.toggleTodo(this.props.id);
-	}
 
 	render() {
 		const completed = this.props.completed?"checked":"";
 		return (
 			<li className="todo-item">
-				<input type="checkbox" checked={completed} name="is_completed" onChange={this.checkChange} />
+				<input type="checkbox" checked={completed} name="is_completed" onChange={() => {TodoActionCreator.toggleTodo(this.props.id);}} />
 				{this.props.itemName}
 				<button className="remove-btn" onClick={() => TodoActionCreator.deleteTodo(this.props.id) }>Remove</button>
 			</li>
