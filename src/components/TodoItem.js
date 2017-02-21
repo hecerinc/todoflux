77a7760f23bec9b17d855dcd1ff9d329ed0bce24
Hector Rincon <hecerinc@hotmail.com>
2017-02-21 15:05:11 -0600

    Add completed style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,8 +5,9 @@ class TodoItem extends React.Component {
 
 	render() {
 		const completed = this.props.completed?"checked":"";
+		const completed_class = this.props.completed ? "todo-item completed":"todo-item";
 		return (
-			<li className="todo-item">
+			<li className={completed_class}>
 				<input type="checkbox" checked={completed} name="is_completed" onChange={() => {TodoActionCreator.toggleTodo(this.props.id);}} />
 				{this.props.itemName}
 				<button className="remove-btn" onClick={() => TodoActionCreator.deleteTodo(this.props.id) }>Remove</button>
